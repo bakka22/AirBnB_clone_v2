ead35322acc07af7e459bebfffa109714ccda904
bakka22 <abobakereltom@gmail.com>
2024-03-24 05:07:41 +0200

    s

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -20,7 +20,7 @@ def c(text):
     return f"C {text}".replace("_", " ")
 
 
-@app.route("/python/", defaults={'text': 'is cool'},strict_slashes=False)
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text):
     return f"Python {text}".replace("_", " ")
