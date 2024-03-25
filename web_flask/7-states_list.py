de022184673c92d2faadc375053480ecabfb6d29
bakka22 <abobakereltom@gmail.com>
2024-03-25 09:48:36 +0200

    s

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,17 +2,15 @@
 """ starts a Flask web application """
 from flask import Flask, render_template
 from models import storage
+from models import *
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def hello():
     """ print a list of states """
-    global storage
     states = storage.all("State")
-    state_list = []
-    for k, state in states.items():
-        state_list.append(state)
+    state_list = list(states.values())
     state_list.sort(key=lambda x: x.name)
     return render_template("7-states_list.html", state_list=state_list)
 
@@ -23,4 +21,4 @@ def reset(exception):
 
 
 if __name__ == ('__main__'):
-    app.run()
+    app.run(host='0.0.0.0', port='5000')
